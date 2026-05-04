fcb632d4165f902066e83ac18eeb6b86f9db741c
clelong14 <colynne.lelong.pro@gmail.com>
2026-05-04 10:32:20 +0200

    fix: redirection ajouter pour accueil

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -16,11 +16,12 @@ import TermsOfUse        from '@/views/TermsOfUse.vue'
 import ContactPage       from '@/views/ContactPage.vue'
 
 const routes = [
+
   { path: '/login', component: LoginView, meta: { public: true } },
   { path: '/register', component: RegisterView, meta: { public: true } },
 
   // 1. La page d'atterrissage pour les visiteurs (Non connectés)
-  { path: '/accueil', component: PublicHomeView, meta: { requiresAuth: false, isPublic: true } },
+  { path:'/', redirect: '/accueil', component: PublicHomeView, meta: { requiresAuth: false, isPublic: true } },
 
   // 2. Les pages d'infos (Rendues publiques : requiresAuth à false)
   // Dans router/index.js
